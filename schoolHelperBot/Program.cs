b634660879e609e4823caa135a9dad3fe68c83fa
shadowAnonim <59304755+shadowAnonim@users.noreply.github.com>
2021-01-31 12:26:40 +0300

    Update Program.cs

--- schoolHelperBot/Program.cs
+++ schoolHelperBot/Program.cs
@@ -26,42 +26,6 @@ namespace schoolHelperBot
             bot.SendTextMessageAsync(chatId, e.Message.Text);
         }
 
-        //async static void Bw_DoWork(object sender, DoWorkEventArgs e)
-        //{
-        //    var worker = sender as BackgroundWorker;
-        //    var key = e.Argument as String;
-        //    try
-        //    {
-        //        await bot.SetWebhookAsync("");
-        //        //Bot.SetWebhook(""); // Обязательно! убираем старую привязку к вебхуку для бота
-        //        int offset = 0; // отступ по сообщениям
-        //        while (true)
-        //        {
-        //            var updates = await bot.GetUpdatesAsync(offset); // получаем массив обновлений
-
-        //            foreach (var update in updates) // Перебираем все обновления
-        //            {
-        //                offset = update.Id + 1;
-        //                var message = update.Message;
-        //                Console.WriteLine(message.Text);
-        //                //if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
-        //                //{
-        //                //    if (message.Text == "/saysomething")
-        //                //    {
-        //                //        // в ответ на команду /saysomething выводим сообщение
-        //                //        await bot.SendTextMessageAsync(message.Chat.Id, "тест", replyToMessageId: message.MessageId);
-        //                //    }
-        //                //}
-
-        //                await bot.SendTextMessageAsync(message.Chat.Id, message.Text);
-        //            }
-
-        //        }
-        //    }
-        //    catch (Telegram.Bot.Exceptions.ApiRequestException ex)
-        //    {
-        //        Console.WriteLine(ex.Message); // если ключ не подошел - пишем об этом в консоль отладки
-        //    }
-        //}
+        
     }
 }
